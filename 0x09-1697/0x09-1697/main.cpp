0eba6a965cd3ea13b8049dce048743aac7cde2ff
DohwanKim <windboy2004@naver.com>
2020-10-30 14:44:02 +0900

    0x09+0x0A

--- 0x09-1697/0x09-1697/main.cpp
+++ 0x09-1697/0x09-1697/main.cpp
@@ -1,10 +1,30 @@
 #include <iostream>
 #include <queue>
 using namespace std;
+int dist[100002];
+int n, k;
 
 int main(int argc, const char * argv[]) {
     ios::sync_with_stdio(0);cin.tie(0);
+    queue<int> q;
     
+    cin >> n >> k;
+    fill(dist, dist+100001, -1);
+    dist[n] = 0;
+    q.push(n);
+    
+    while (dist[k] == -1) {
+        int cur = q.front(); q.pop();
+        for(int nxt: {cur-1, cur+1, cur*2}) {
+            if(nxt < 0 || nxt > 100000) continue;
+            if(dist[nxt] != -1) continue;
+            
+            dist[nxt] = dist[cur]+1;
+            q.push(nxt);
+        }
+    }
+    
+    cout << dist[k] << '\n';
     
     return 0;
 }
